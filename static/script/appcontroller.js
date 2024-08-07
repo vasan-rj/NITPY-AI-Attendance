cf6d7e0b304db2369447b48faaf52a461600f69b
Hemanth Kumar Posa <posahemanth@gmail.com>
2024-08-07 12:08:45 +0530

    modifiedControllers

--- static/script/appcontroller.js
+++ static/script/appcontroller.js
@@ -16,3 +16,25 @@ app.controller('LoginController', ['$scope', '$location', function($scope, $loca
         window.location.href = '/facultypage';
     };
 }]);
+
+app.controller('FacultypageController', function($scope) {
+    $scope.attendance = {};
+    $scope.students = [
+        { rollNo: 'CS21B10XX', status: 'Absent', change: false },
+        { rollNo: 'CS21B10YY', status: 'Absent', change: false },
+        { rollNo: 'CS21B10ZZ', status: 'Absent', change: false }
+    ];
+
+    $scope.takeAttendance = function() {
+        alert('Attendance taken for ' + $scope.attendance.classRoom);
+    };
+
+    $scope.commitChange = function() {
+        $scope.students.forEach(function(student) {
+            if (student.change) {
+                student.status = student.status === 'Absent' ? 'Present' : 'Absent';
+            }
+        });
+        alert('Changes committed');
+    };
+});
